744eb441789fa10ee2fb8262da44cfbcea31e724
JBurgin9 <justinburgin@pursuit.org>
2019-05-15 08:23:12 -0400

    Sixth commit; minor code addition

--- app/src/main/java/org/quietlip/voicescapstone/SignInActivity.java
+++ app/src/main/java/org/quietlip/voicescapstone/SignInActivity.java
@@ -1,13 +1,58 @@
 package org.quietlip.voicescapstone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class SignInActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText usernameInputET, passwordInputET;
+    private Button loginBtn;
+    private ImageView logoIV;
+    private TextView signUpTV;
+
+    private final List<AuthUI.IdpConfig> loginProviders = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.PhoneBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        initViews();
+
+
+        signUpTV.setOnClickListener(this);
+        loginBtn.setOnClickListener(this);
+    }
+
+    //initViews
+    private void initViews(){
+        usernameInputET = findViewById(R.id.username_et);
+        passwordInputET = findViewById(R.id.password_et);
+        loginBtn = findViewById(R.id.log_in_button);
+        logoIV = findViewById(R.id.logo_iv);
+        signUpTV = findViewById(R.id.sign_up_tv);
+    }
+
+    @Override
+    public void onClick(View v) {
+    switch (v.getId()){
+        case R.id.sign_up_tv:
+
+            break;
+        case R.id.log_in_button:
+
+            break;
+    }
     }
 }
