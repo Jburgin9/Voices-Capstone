a76e85d41083a5020f3d07ab296e3e41eca8655f
lindapokorny <lindapokorny@pursuit.org>
2019-05-14 22:02:17 -0400

    set OnClickListener for logIn Button

--- app/src/main/java/org/quietlip/voicescapstone/SplashActivity.java
+++ app/src/main/java/org/quietlip/voicescapstone/SplashActivity.java
@@ -1,5 +1,6 @@
 package org.quietlip.voicescapstone;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ public class SplashActivity extends AppCompatActivity {
     private Button logInButton;
     private Button signUpButton;
     private GifImageView splash_image;
+    private ImageView mouth_after;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +25,27 @@ public class SplashActivity extends AppCompatActivity {
         logInButton = findViewById(R.id.log_in_button);
         signUpButton = findViewById(R.id.sign_up_button);
         splash_image = findViewById(R.id.gifImageView);
+        mouth_after = findViewById(R.id.mouth_after);
         int secondsDelayed = 1;
         new Handler().postDelayed(new Runnable() {
             public void run() {
+                mouth_after.setVisibility(View.VISIBLE);
                 logInButton.setVisibility(View.VISIBLE);
                 signUpButton.setVisibility(View.VISIBLE);
+
                 //   startActivity(new Intent(SplashActivity.this,
                 //         MainActivity.class));
                 // finish();
             }
         }, secondsDelayed * 4000);
+logInButton.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+Intent intent = new Intent(this, SignUp.class);
+startActivity(intent);
 
     }
+});
+    }
+
 }
