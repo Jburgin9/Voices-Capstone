46c495e150be882014609807c127b1767e4d6d4d
fribelguzman <fribelguzman@pursuit.org>
2019-05-16 20:25:51 -0400

    recordactivity

--- app/src/main/java/org/quietlip/voicescapstone/models/AudioModel.java
+++ app/src/main/java/org/quietlip/voicescapstone/models/AudioModel.java
@@ -1,4 +1,22 @@
 package org.quietlip.voicescapstone.models;
 
 public class AudioModel {
+    private String audioFile;
+    private String audioTitle;
+
+    public String getAudioFile() {
+        return audioFile;
+    }
+
+    public void setAudioFile(String audioFile) {
+        this.audioFile = audioFile;
+    }
+
+    public String getAudioTitle() {
+        return audioTitle;
+    }
+
+    public void setAudioTitle(String audioTitle) {
+        this.audioTitle = audioTitle;
+    }
 }
--- app/src/main/java/org/quietlip/voicescapstone/models/UserModel.java
+++ app/src/main/java/org/quietlip/voicescapstone/models/UserModel.java
@@ -1,4 +1,9 @@
 package org.quietlip.voicescapstone.models;
 
+import java.util.List;
+
 public class UserModel {
+    private String userName;
+    private List <AudioModel> audioList;
+
 }
