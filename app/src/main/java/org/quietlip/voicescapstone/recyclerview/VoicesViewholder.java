fffa6200efd98d597054b1e13f15f7e315089b81
fribelguzman <fribelguzman@pursuit.org>
2019-05-17 19:12:50 -0400

    recycler

--- app/src/main/java/org/quietlip/voicescapstone/recyclerview/VoicesViewholder.java
+++ app/src/main/java/org/quietlip/voicescapstone/recyclerview/VoicesViewholder.java
@@ -16,8 +16,8 @@ public class VoicesViewholder extends RecyclerView.ViewHolder {
     public VoicesViewholder(@NonNull View itemView) {
         super(itemView);
 
-        play=itemView.findViewById(R.id.play);
-        title =itemView.findViewById(R.id.title);
+        play=itemView.findViewById(R.id.play_button_item_view);
+        title =itemView.findViewById(R.id.name_item_view);
     }
 
 
