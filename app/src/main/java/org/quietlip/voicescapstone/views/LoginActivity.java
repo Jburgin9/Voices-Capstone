70152419dd32675ee3ea52638d060984eadb6cd6
Justin Burgin <justinburgin@pursuit.org>
2019-05-18 16:35:56 -0400

    Second commit; login -> profile

--- app/src/main/java/org/quietlip/voicescapstone/views/LoginActivity.java
+++ app/src/main/java/org/quietlip/voicescapstone/views/LoginActivity.java
@@ -88,6 +88,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             if (task.isSuccessful()) {
                                 helper.dismissFirelog();
                                 helper.makeSnackie(coord, "success");
+                                startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
                             } else {
                                 helper.dismissFirelog();
                                 helper.makeSnackie(coord, "failure");
